b5237c9558aae0c7df062fd5609be726b537ac93
maxzza5 <154840156+maxzza5@users.noreply.github.com>
2023-12-26 04:35:59 +0700

    Update condb.php

--- condb.php
+++ condb.php
@@ -1,5 +1,5 @@
 <?php
-$condb= mysqli_connect("localhost","root","","workshop_work_io") or die("Error: " . mysqli_error($condb));
+$condb= mysqli_connect("localhost","root","","A4PD") or die("Error: " . mysqli_error($condb));
 mysqli_query($condb, "SET NAMES 'utf8' ");
 date_default_timezone_set('Asia/Bangkok');
 ?>
